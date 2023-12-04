83f716e3409dfd121b0e85c246c77f1619ce6e04
ismael <imolina218@gmail.com>
2023-12-04 05:13:22 -0300

    feat: a lot of things

--- main.js
+++ main.js
@@ -1,21 +1,125 @@
 import gsap from "gsap"
+import ScrollTrigger from "gsap/dist/ScrollTrigger"
 import Lenis from '@studio-freight/lenis'
 import MouseFollower from "mouse-follower"
 
 import './style.scss'
 import './cursor.scss'
 
+gsap.registerPlugin(ScrollTrigger)
+
 // LENIS SETUP
 const lenis = new Lenis()
 
 function raf(time) {
   lenis.raf(time)
+  ScrollTrigger.update()
   requestAnimationFrame(raf)
 }
 
 requestAnimationFrame(raf)
 
-// BLOB CURSOR FOLLOWER SETUP
+// LENIS ANCHOR
+const aboutLink = document.querySelector(".about-link")
+const workLink = document.querySelector(".work-link")
+const contactLink = document.querySelector(".contact-link")
+const aboutEl = document.querySelector("#about-us")
+const workEl = document.querySelector("#our-work")
+const contactEl = document.querySelector("#contact")
+
+aboutLink.addEventListener("click", () => {
+  lenis.scrollTo(aboutEl)
+
+  aboutLink.classList.add("active")
+  workLink.classList.remove("active")
+  contactLink.classList.remove("active")
+})
+workLink.addEventListener("click", () => {
+  lenis.scrollTo(workEl)
+
+  workLink.classList.add("active");
+  aboutLink.classList.remove("active")
+  contactLink.classList.remove("active")
+})
+contactLink.addEventListener("click", () => {
+  lenis.scrollTo(contactEl)
+
+  contactLink.classList.add("active");
+  aboutLink.classList.remove("active")
+  workLink.classList.remove("active")
+})
+
+// GSAP BACKGROUND COLOR CHANGES
+const container = document.querySelector(".main")
+const hero = document.querySelector(".hero")
+const fadeHero = document.querySelector(".fade-hero-section")
+const about = document.querySelector(".about-section")
+const howWeWork = document.querySelector(".how-we-work")
+const contact = document.querySelector(".contact")
+
+ScrollTrigger.create( {
+  trigger: fadeHero,
+  start: 'top top',
+  end: 'bottom top',
+  onEnter: () => gsap.to(container, {backgroundColor:"#EE512F", duration: 1, ease:"ease.in"}),
+  onLeave: () => gsap.to(container, {backgroundColor:"#108896", duration: 1, ease:"ease.out"}),
+  onLeaveBack: () => gsap.to(container, {backgroundColor:"#EE512F", duration: 1, ease:"ease.in"}),
+  onEnterBack: () => gsap.to(container, {backgroundColor:"#EE512F", duration: 1, ease:"ease.in"})
+})
+ScrollTrigger.create( {
+  trigger: about,
+  start: 'top top',
+  end: '+=500%',
+  onEnter: () => gsap.to(container, {backgroundColor:"#108896", duration: 1, ease:"ease.in"}),
+  onLeave: () => gsap.to(container, {backgroundColor:"#1C374D", duration: 1, ease:"ease.out"}),
+  onLeaveBack: () => gsap.to(container, {backgroundColor:"#EE512F", duration: 1, ease:"ease.in"}),
+  onEnterBack: () => gsap.to(container, {backgroundColor:"#108896", duration: 1, ease:"ease.in"})
+})
+ScrollTrigger.create( {
+  trigger: howWeWork,
+  start: '+=40%',
+  end: '+=1520%',
+  onEnter: () => gsap.to(container, {backgroundColor:"#1C374D", duration: 1, ease:"ease.in"}),
+  onLeave: () => gsap.to(container, {backgroundColor:"#FCA720", duration: 1, ease:"ease.out"}),
+  onLeaveBack: () => gsap.to(container, {backgroundColor:"#108896", duration: 1, ease:"ease.in"}),
+  onEnterBack: () => gsap.to(container, {backgroundColor:"#1C374D", duration: 1, ease:"ease.in"})
+})
+
+// GSAP HORIZONTAL SCROLL
+//MEMBERS
+const membersSection = document.querySelector(".members")
+let sectionItems = gsap.utils.toArray(".member-item")
+
+gsap.to(sectionItems, {
+  xPercent: -100 * (sectionItems.length - 1),
+  ease: "sine.out",
+  scrollTrigger: {
+    trigger: membersSection,
+    pin: true,
+    scrub: 2,
+    snap: 1 / (sectionItems.length - 1),
+    end: "+=" + membersSection.offsetWidth
+  }
+})
+
+// HOW WE WORK
+const howWeWorkSection = document.querySelector(".our-way-wrapper")
+let howWeWorkItems = gsap.utils.toArray(".how-we-work-item")
+
+gsap.to(howWeWorkItems, {
+  xPercent: -120.9 * (howWeWorkItems.length - 1),
+  ease: "sine.out",
+  scrollTrigger: {
+    trigger: howWeWorkSection,
+    pin: true,
+    scrub: 2,
+    end: "+=" + howWeWorkSection.offsetWidth
+  }
+})
+
+
+
+// BLOB CURSOR FOLLOWER SETUP ANS STATE MANAGEMENT
 MouseFollower.registerGSAP(gsap);
 
 const cursor = new MouseFollower({
