bedd06e642c57f5ef3e0247b69262a3ad71e7b1b
ismael <imolina218@gmail.com>
2024-01-04 16:20:12 -0300

    fix: hero inicial animation sequence, last word translate up and asterisk anim

--- src/js/videomuting.js
+++ src/js/videomuting.js
@@ -9,29 +9,44 @@ function videoMuting() {
     videoPlaying = this;
   };
 
-  // init event handler
-  const videos = document.querySelectorAll(".card video");
-  for (let i = 0; i < videos.length; i++) {
-    videos[i].addEventListener("play", onPlay);
-  }
-
   // get all iframes in the scene
   let allIframesOnScene = document.getElementsByTagName("iframe");
+
+  // init event handler
+  // const videos = document.querySelectorAll(".card video");
+  /* for (let i = 0; i < allIframesOnScene.length; i++) {
+    allIframesOnScene[i].addEventListener("click", onPlay);
+    console.log(allIframesOnScene[i]);
+  } */
+  /* for(let i = 0; i < allIframesOnScene.length; i++) {
+    // filtra elementos que no contengan player.vimeo.com en el src
+    if(allIframesOnScene[i].src == null || allIframesOnScene[i].src.indexOf("player.vimeo.com") == -1) {
+      continue;
+    }
+    
+    let iframe = allIframesOnScene[i];
+    let player = new Vimeo.Player(iframe);
+    
+      player.on('pause', function() {
+        console.log('paused');
+      });
+    
+    player.pause();
+  } */
 
   let closebtn = document.querySelector(".js-close");
   closebtn.addEventListener("click", function (e) { 
     /* document.querySelectorAll("video").forEach((v) => {
         v.pause();
     }); */
-
-    for(var i = 0; i < allIframesOnScene.length; i++) {
+    for(let i = 0; i < allIframesOnScene.length; i++) {
       // filtra elementos que no contengan player.vimeo.com en el src
       if(allIframesOnScene[i].src == null || allIframesOnScene[i].src.indexOf("player.vimeo.com") == -1) {
         continue;
       }
       
-      var iframe = allIframesOnScene[i];
-      var player = new Vimeo.Player(iframe);
+      let iframe = allIframesOnScene[i];
+      let player = new Vimeo.Player(iframe);
       
           player.on('pause', function() {
           console.log('paused');
